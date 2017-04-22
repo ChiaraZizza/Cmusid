be34b43a4dcffb65a065e166868c6585f8c04d86
Alex Mitchell <alex@alexthemitchell.com>
2017-04-22 13:01:50 -0500

    Add basic file reading

--- pan/src/main.rs
+++ pan/src/main.rs
@@ -1,3 +1,20 @@
+extern crate chromaprint;
+
+use std::fs::File;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+  read()
+}
+
+fn read() {
+  let file = File::open("./input/cool.flac");
+  match file {
+    Ok(mut f) => {
+      let mut vec = Vec::new();
+      f.read_to_end(&mut vec);
+      println!("{:?}",vec);
+    },
+    Err(e) => println!("{}",e)
+  }
 }
