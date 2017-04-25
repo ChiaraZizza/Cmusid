4b1c9dff777bc9c9669f2153075ddeedb4b6bfa0
Alex Mitchell <alex@alexthemitchell.com>
2017-04-25 09:36:52 -0500

    Switch to C

---
+++ pan/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <chromaprint.h>
+
+const int sample_rate = 44100;
+const int num_channels = 2;
+const int FILE_BLOCK_SIZE = 2;
+
+void fingerprintFile(FILE *file) {
+  ChromaprintContext *ctx;
+  ctx = chromaprint_new(CHROMAPRINT_ALGORITHM_DEFAULT);
+
+  chromaprint_start(ctx,sample_rate,num_channels);
+  int16_t *buffer = (int16_t*) malloc((FILE_BLOCK_SIZE * sizeof(int16_t)) + 1);
+  while (fread(buffer,FILE_BLOCK_SIZE, 1, file)) {
+    chromaprint_feed(ctx,buffer,FILE_BLOCK_SIZE);
+  }
+  chromaprint_finish(ctx);
+
+  char *fingerprint;
+  chromaprint_get_fingerprint(ctx, &fingerprint);
+  printf("%s\n", fingerprint);
+
+  chromaprint_free(ctx);
+}
+
+int main () {
+  FILE *file;
+  file = fopen("", "r");
+  fingerprintFile(file);
+  return 0;
+}
