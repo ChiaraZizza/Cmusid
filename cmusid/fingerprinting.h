7f13c650dbe34df5ded6f3e8f72ec375a7342ce3
Alex Mitchell <alex@alexthemitchell.com>
2017-05-07 16:05:49 -0500

    Fix makefile!

--- cmusid/fingerprinting.h
+++ cmusid/fingerprinting.h
@@ -1,7 +1,12 @@
+#ifndef __FINGERPRINTING_H__
+#define __FINGERPRINTING_H__
+
 typedef struct FileNode {
   char * filename;
   char * fingerprint;
   int duration;
 } FileNode_t;
 
-int fingerprintFile(char* filePath, char** fingerprint);
+extern int fingerprintFile(char* filePath, char** fingerprint);
+
+#endif // __FINGERPRINTING_H__
