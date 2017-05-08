acd6f180cda9c99e4d48e60de623df62a9863d8a
Alex Mitchell <alex@alexthemitchell.com>
2017-05-08 18:23:48 -0500

    Fix memory leak

--- cmusid/fingerprinting.c
+++ cmusid/fingerprinting.c
@@ -1,3 +1,5 @@
+#include "fingerprinting.h"
+
 #include <assert.h>
 #include <chromaprint.h>
 #include <FLAC/stream_decoder.h>
@@ -6,11 +8,10 @@
 #include <string.h>
 #include <pthread.h>
 
-#include "fingerprinting.h"
-
 #define SAMPLE_RATE 44100
 #define NUM_CHANNELS 2
 
+// Chromaprint's sample app fpcalc only samples the first two minutes.
 #define MAX_DURATION 120
 #define NUM_THREADS 4
 
@@ -21,8 +22,12 @@ typedef struct arg
   int totalNumFiles;
 } arg_t;
 
+/// We will only fingerprint the first MAX_DURATION seconds
 long long int streamRemaining = MAX_DURATION * SAMPLE_RATE;
 
+/**
+ * A data-read function for libFLAC which feeds into the Chromaprint context given
+ */
 FLAC__StreamDecoderWriteStatus
 processFrame (const FLAC__StreamDecoder * decoder, const FLAC__Frame * frame,
 	      const FLAC__int32 * const buffer[], void *context)
@@ -35,8 +40,8 @@ processFrame (const FLAC__StreamDecoder * decoder, const FLAC__Frame * frame,
   if (!chromaprint_feed
       (context, buffer, frame->header.sample_rate * channels))
     {
-      fprintf (stderr, "Error feeding Chromaprint from buffer\n");
-      return FLAC__STREAM_DECODER_WRITE_STATUS_ABORT;
+      fprintf (stderr, "Error feeding Chromaprint from libFLAC buffer\n");
+      exit (2);
     }
   if (streamRemaining < 0)
     {
@@ -45,19 +50,26 @@ processFrame (const FLAC__StreamDecoder * decoder, const FLAC__Frame * frame,
   return FLAC__STREAM_DECODER_WRITE_STATUS_CONTINUE;
 }
 
+/**
+ * A data-read error handler for libFLAC
+ */
 void
 processError (const FLAC__StreamDecoder * decoder,
 	      FLAC__StreamDecoderErrorStatus status, void *context)
 {
-  printf ("There was an error in FLAC decoding: %d\n", status);
+  fprintf (stderr, "There was an error in FLAC decoding: %d\n", status);
+  exit (2);
 }
 
+/**
+ * Uses Chromaprint and libFLAC to fingerprint the file at filePath
+ * Returns the duration of the track
+ */
 int
 fingerprintFile (char *filePath, char **fingerprint)
 {
   FLAC__StreamDecoder *decoder = FLAC__stream_decoder_new ();
-  ChromaprintContext *ctx;
-  ctx = chromaprint_new (CHROMAPRINT_ALGORITHM_DEFAULT);
+  ChromaprintContext *ctx = chromaprint_new (CHROMAPRINT_ALGORITHM_DEFAULT);
 
   if (!chromaprint_start (ctx, SAMPLE_RATE, NUM_CHANNELS))
     {
@@ -79,14 +91,16 @@ fingerprintFile (char *filePath, char **fingerprint)
       exit (2);
     }
 
+  // This is where the magic happens!
   if (!FLAC__stream_decoder_process_until_end_of_stream (decoder))
     {
       FLAC__StreamDecoderState state =
 	FLAC__stream_decoder_get_state (decoder);
+      // It is normal if we reach the end of stream or if we aborted
       if (!(state == FLAC__STREAM_DECODER_END_OF_STREAM ||
-            state == FLAC__STREAM_DECODER_ABORTED))
+	    state == FLAC__STREAM_DECODER_ABORTED))
 	{
-    fprintf(stderr, "Error doing something: %d\n", state);
+	  fprintf (stderr, "Error decoding FLAC stream: %d\n", state);
 	}
     }
 
@@ -95,13 +109,18 @@ fingerprintFile (char *filePath, char **fingerprint)
       fprintf (stderr, "Error finishing Chromaprint feed\n");
       exit (2);
     }
-  assert (FLAC__stream_decoder_finish (decoder));
+  if (!(FLAC__stream_decoder_finish (decoder)))
+    {
+      fprintf (stderr, "Error finishing FLAC decoding\n");
+      exit (2);
+    }
   if (!chromaprint_get_fingerprint (ctx, fingerprint))
     {
       fprintf (stderr, "Error retrieving fingerprint from Chromaprint\n");
       exit (2);
     }
   chromaprint_free (ctx);
+  FLAC__stream_decoder_delete(decoder);
   return -1;
 }
 
@@ -144,4 +163,3 @@ fingerprintFilesInParallel (FileNode_t * files, int numFiles)
 	}
     }
 }
-
