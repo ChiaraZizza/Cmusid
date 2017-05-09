d3a5b16cae25225b84320e3074d17da5c7781f47
Maddie <kirwinma@grinnell.edu>
2017-05-08 21:21:45 -0500

    huzzah: add flac.c

--- cmusid/flac.c
+++ cmusid/flac.c
@@ -4,145 +4,131 @@
 #include <stdio.h>
 #include <string.h>
 
-void
-practice (FLAC__Metadata_SimpleIterator * flac_iter, bool use, char *title,
-	  char *album, char *artist)
+void renameVorbis (FLAC__Metadata_SimpleIterator * flac_iter, char *title,
+        char *album, char *artist)
 {
+    int tlen = strlen (title);
+    int allen = strlen (album);
+    int arlen = strlen (artist);
 
-  int tlen = strlen (title);
-  int allen = strlen (album);
-  int arlen = strlen (artist);
-
-  // iterate while there are blocks to read
-  while (FLAC__metadata_simple_iterator_next (flac_iter))
+    // iterate while there are blocks to read
+    while (FLAC__metadata_simple_iterator_next (flac_iter))
     {
-      FLAC__StreamMetadata *meta =
-	FLAC__metadata_simple_iterator_get_block (flac_iter);
-      FLAC__StreamMetadata *temp =
-	FLAC__metadata_object_new (FLAC__METADATA_TYPE_VORBIS_COMMENT);
+        // meta points at the current flac file
+        FLAC__StreamMetadata *meta =
+            FLAC__metadata_simple_iterator_get_block (flac_iter);
 
+        // temp is a new metadata object to populate
+        FLAC__StreamMetadata *temp =
+            FLAC__metadata_object_new (FLAC__METADATA_TYPE_VORBIS_COMMENT);
 
-      printf ("meta contents: %u\n", meta->type);
-      if (meta->type == FLAC__METADATA_TYPE_VORBIS_COMMENT)
-	{
-	  FLAC__StreamMetadata_VorbisComment_Entry *t =
-	    (FLAC__StreamMetadata_VorbisComment_Entry *)
-	    malloc (sizeof (FLAC__StreamMetadata_VorbisComment_Entry) * 3);
+        if (meta->type == FLAC__METADATA_TYPE_VORBIS_COMMENT)
+        {
+            // empty vorbis comment entry to populate
+            FLAC__StreamMetadata_VorbisComment_Entry *new_entry =
+                (FLAC__StreamMetadata_VorbisComment_Entry *)
+                malloc (sizeof (FLAC__StreamMetadata_VorbisComment_Entry) * 3);
 
-	  //temp->data.vorbis_comment.comments = (FLAC__StreamMetadata_VorbisComment_Entry*)malloc(sizeof(FLAC__StreamMetadata_VorbisComment_Entry) * 3);
+            for (int i = 0; i < meta->data.vorbis_comment.num_comments; i++)
+            {
 
-	  t[0].entry = (FLAC__byte *) malloc (sizeof (FLAC__byte) * tlen);
-	  t[1].entry = (FLAC__byte *) malloc (sizeof (FLAC__byte) * allen);
-	  t[2].entry = (FLAC__byte *) malloc (sizeof (FLAC__byte) * arlen);
+                // Copy the function arguments to FLAC__byte* to insert into empty vorbis comment entry
+                FLAC__byte *title_bytes =
+                    malloc (sizeof (FLAC__byte) * tlen + 1);
+                memcpy (title_bytes, title, tlen);
+                title_bytes[tlen] = '\0';
 
-	  //FLAC__StreamMetadata_VorbisComment vorbis = meta->data.vorbis_comment;
+                FLAC__byte *album_bytes =
+                    malloc (sizeof (FLAC__byte) * allen + 1);
+                memcpy (album_bytes, album, allen);
+                album_bytes[allen] = '\0';
 
-	  printf ("num_comments: %d\n",
-		  meta->data.vorbis_comment.num_comments);
+                FLAC__byte *artist_bytes =
+                    malloc (sizeof (FLAC__byte) * arlen + 1);
+                memcpy (artist_bytes, artist, arlen);
+                artist_bytes[arlen] = '\0';
 
-	  for (int i = 0; i < meta->data.vorbis_comment.num_comments; i++)
-	    {
-	      printf ("%s\n", meta->data.vorbis_comment.comments[i].entry);
+                // Populate the new vorbis comment entry
+                new_entry[0].length = tlen;
+                new_entry[0].entry = title_bytes;
 
-	      if (use)
-		{
+                new_entry[1].length = allen;
+                new_entry[1].entry = album_bytes;
 
-		  FLAC__byte *title_bytes =
-		    malloc (sizeof (FLAC__byte) * tlen + 1);
-		  memcpy (title_bytes, title, tlen);
-		  title_bytes[tlen + 1] = '\0';
+                new_entry[2].length = arlen;
+                new_entry[2].entry = artist_bytes;
 
-		  FLAC__byte *album_bytes =
-		    malloc (sizeof (FLAC__byte) * allen + 1);
-		  memcpy (album_bytes, album, allen);
-		  album_bytes[allen + 1] = '\0';
+                temp->data.vorbis_comment.comments = new_entry;
+                temp->data.vorbis_comment.num_comments = 3;
 
-		  FLAC__byte *artist_bytes =
-		    malloc (sizeof (FLAC__byte) * arlen + 1);
-		  memcpy (artist_bytes, artist, arlen);
-		  artist_bytes[arlen + 1] = '\0';
-
-		  //create vorbis entry and populate it
-		  t[0].length = tlen;
-		  t[0].entry = title_bytes;
-
-		  t[1].length = allen;
-		  t[1].entry = album_bytes;
-
-		  t[2].length = arlen;
-		  t[2].entry = artist_bytes;
-
-		  temp->data.vorbis_comment.comments = t;
-		  temp->data.vorbis_comment.num_comments = 3;
-
-		  FLAC__metadata_object_vorbiscomment_replace_comment (temp,
-								       *t,
-								       false,
-								       false);
-		  assert (FLAC__metadata_simple_iterator_set_block
-			  (flac_iter, temp, false));
-		}
-	    }
-	}
+                FLAC__metadata_object_vorbiscomment_replace_comment (temp, *new_entry, false, true);
+                assert (FLAC__metadata_simple_iterator_set_block
+                        (flac_iter, temp, true));
+            }
+        }
     }
-  printf ("Hello\n");
 }
 
-void
-test ()
+void renameFile(char* old_filename, char* title, char* album, char* artist) {
+    // generates new filename string
+    int tlen, alen, arlen;
+    tlen = strlen(title);
+    alen = strlen(album);
+    arlen = strlen(artist);
+    int new_len = tlen+alen+arlen + 16;
+    char* new = malloc (sizeof(char)*new_len);
+    snprintf(new, new_len, "input/%s--%s--%s.flac", artist, album, title);
+
+    // rename file
+    //ret = rename(old_filename, new);
+    char* buffer = (char*) malloc(sizeof(char) * new_len * new_len);
+    snprintf(buffer, new_len*new_len, "mv %s %s", old_filename, new);
+    assert(system(buffer)==0);
+
+    // free created strings
+    free(new);
+    free(buffer);
+}
+
+    void
+modifyFile(char* title, char* artist, char* album, char* old_filename)
 {
-  char *title = "TITLE=Keep My Cool";
-  char *artist = "ARTIST=the Wellness";
-  char *album = "ALBUM=doubles";
 
-  FLAC__Metadata_SimpleIterator *flac_iter =
-    FLAC__metadata_simple_iterator_new ();
+    // Check if old_filename is a valid path
+    FILE *file = fopen(old_filename, "r");
+    if (file == NULL) {
+        fprintf(stderr, "%s\n", "Please give a valid filepath: FLAC files should be in the 'input' directory.");
+        exit(2);
+    }
+    fclose(file);
+    FLAC__Metadata_SimpleIterator *flac_iter =
+        FLAC__metadata_simple_iterator_new ();
 
-  // Attach iterator to a FLAC file (this function returns a flac bool)
-  if (!FLAC__metadata_simple_iterator_init
-      (flac_iter, "input/cool.flac", false, false))
+    // Attach iterator to a FLAC file 
+    if (!FLAC__metadata_simple_iterator_init
+            (flac_iter, old_filename, false, false))
     {
-      // error message
-      printf ("Error with initializing iterator\n");
+        fprintf(stderr, "%s", "Error with initializing iterator\n");
     }
 
-  // make sure the file is writable
-  if (!FLAC__metadata_simple_iterator_is_writable (flac_iter))
+    // make sure the file is writable
+    if (!FLAC__metadata_simple_iterator_is_writable (flac_iter))
     {
-      // error? flac isn't writable
-      // print something, then exit
-      printf ("iter1 failed\n");
-    }
-
-
-  printf ("First time running:\n");
-  practice (flac_iter, true, title, album, artist);
-  FLAC__metadata_simple_iterator_delete (flac_iter);
-
-
-
-
-  FLAC__Metadata_SimpleIterator *flac_iter2 =
-    FLAC__metadata_simple_iterator_new ();
-
-  // Attach iterator to a FLAC file (this function returns a flac bool)
-  if (!FLAC__metadata_simple_iterator_init
-      (flac_iter2, "input/cool.flac", false, false))
-    {
-      // error message
-      printf ("Error with initializing iterator\n");
-    }
-
-  // make sure the file is writable
-  if (!FLAC__metadata_simple_iterator_is_writable (flac_iter2))
-    {
-      // error? flac isn't writable
-      // print something, then exit
-      printf ("iter2 failed\n");
+        fprintf(stderr, "%s", "flac iterator is not writable\n");
     }
 
 
-  printf ("\nSecond time running:\n");
-  practice (flac_iter2, false, title, album, artist);
+    renameVorbis(flac_iter, title, album, artist);
 
+    // "refreshes" iterator/iterate from the beginning of the flac file again
+    if (!FLAC__metadata_simple_iterator_init
+            (flac_iter, old_filename, false, false))
+    {
+        fprintf(stderr, "%s", "Error with reinitializing iterator\n");
+    }
+
+    FLAC__metadata_simple_iterator_delete(flac_iter);
+
+    // Rename File
+    renameFile(old_filename, title, album, artist);
 }
